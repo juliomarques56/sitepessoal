c70b9d977d8fa4cfa967859443d080afb70c93cd
Júlio Marques <jotamarques56@gmail.com>
2020-04-22 18:30:55 -0400

    Edit backtop fonts

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,6 @@ const toggler = document.querySelector('.menu__toggler');
 const menu    = document.querySelector('.menu');
 //const link    = document.querySelectorAll('.link')
 
-/*
- * Toggles on and off the 'active' class on the menu
- * and the toggler button.
- */
 toggler.addEventListener('click', () => {
   toggler.classList.toggle('active');
   menu.classList.toggle('active');
@@ -20,7 +16,23 @@ $(function(){
   });
 });
 
+$(document).ready(function(){
+  $('.backtop').hide();
 
+  $(window).scroll(function(){
+    if($(this).scrollTop()>0){
+      $('.backtop').fadeIn();
+    }else{
+      $('.backtop').fadeOut();
+    }
+  });
+
+  $('.backtop').click(function(){
+    $('html, body').animate({
+      scrollTop: 0
+    }, 600);
+  })
+});
 
 
 $('nav a').click(function(e) {
@@ -33,42 +45,3 @@ $('nav a').click(function(e) {
   
 });
 
-debounce = function(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
-
-(function() {
-var $target = $('.pt');
-var $offset = $(window).height() * 2/6;
-
-function animeScroll(){
-  var documentTop = $(document).scrollTop();
-  console.log(documentTop);
-  $target.each(function() {
-    var itemTop = $(this).offset().top;
-    if(documentTop > itemTop - $offset){
-      $('.anime').css({opacity: '0', transition: '0.2s'});
-    } else{
-      $('.anime').css('opacity', '1');
-    }
-
-  })
-}
-
-animeScroll();
-
-$(document).scroll(debounce(function(){
-  animeScroll();
-}, 100));
-}());
