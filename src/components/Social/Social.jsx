75d3ca40562d8d9b677c4dc80380612ddddf6d4d
juliomarques56 <40442660+juliomarques56@users.noreply.github.com>
2020-06-18 17:16:02 -0400

    Update Social.jsx

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -8,10 +8,10 @@ function Social() {
         <section className="social__container">
             <div className="social__link">
                 <div className="top__line"></div>
-                <a href="https://github.com/juliomarques56"  target="__blank" title="Github" className="github"><i className="fab fa-github" aria-hidden="true"></i></a>
-                <a href="https://www.facebook.com/jota56"  target="__blank" title="Facebook" className="facebook"><i className="fab fa-facebook" aria-hidden="true"></i></a>
-                <a href="https://www.linkedin.com/in/julio-marques-2772a6b2/"  target="__blank" title="Linkedin" className="linkedin "><i className="fab fa-linkedin-in" aria-hidden="true"></i></a>
-                <a href="https://www.instagram.com/julio_marques56/?hl=pt-br"  target="__blank" title="Instagram" className="instagram"><i className="fab fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://github.com/juliomarques56"  target="__blank" title="Github" className="github"><i className="fab fa-github" aria-hidden="true"></i>.</a>
+                <a href="https://www.facebook.com/jota56"  target="__blank" title="Facebook" className="facebook"><i className="fab fa-facebook" aria-hidden="true"></i>.</a>
+                <a href="https://www.linkedin.com/in/julio-marques-2772a6b2/"  target="__blank" title="Linkedin" className="linkedin "><i className="fab fa-linkedin-in" aria-hidden="true"></i>.</a>
+                <a href="https://www.instagram.com/julio_marques56/?hl=pt-br"  target="__blank" title="Instagram" className="instagram"><i className="fab fa-instagram" aria-hidden="true"></i>.</a>
                 <div className="botton__line"></div>
             </div> 
       </section> 
