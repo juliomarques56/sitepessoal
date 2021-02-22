be9add160c055fa6b6b1d41ea9d54966b71d0dd7
juliomarques56 <40442660+juliomarques56@users.noreply.github.com>
2021-02-22 09:56:38 -0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,14 +11,14 @@ function About() {
             </div>
             <div className="about__content">
                 <h2>Olá!</h2>
-                <p>Meu nome é Júlio Marques, casado, finalizando graduação em Análise e Desenvolvimento de Sistemas. Atualmente moro em Manaus-AM, mas,
-                    logo estarei mudando para Rio Grande do Sul na busca por novas oportunidades e desafios. Trabalho na área de TI desde 2013.
+                <p>Meu nome é Júlio Marques, casado, finalizando graduação em Análise e Desenvolvimento de Sistemas
                 </p>
                 <p>
+                    Meu nome é Júlio Marques, casado, com graduação em Análise e Desenvolvimento de Sistemas
                     Atualmente estou trabalhando na Rapport Soluções como autônomo em modelo home office. Presto suporte aos clientes do sistema, além de configurar, testar e homologar funcionalidades novas e atuais, investigar, analisar e resolver problemas referente ao sistema.
                 </p>
                 <p>
-                    Em paralelo venho estudando e praticando sobre desenvolvimento front-end. Meu objetivo é ter a oportunidade de ingressar na área de desenvolvimento.
+                    Em paralelo venho estudando e praticando sobre teste de software e ter a oportunidade de ingressar na área teste.
                 </p>
             </div>
         </div>
